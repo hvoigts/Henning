662af700043399bb9aee0598cdf536ff3ded845e
jtuttas <jtuttas@gmx.net>
2020-05-06 16:48:22 +0200

    Mit git ignore

--- src/app/ToFastException.java
+++ src/app/ToFastException.java
@@ -1,9 +1,15 @@
 package app;
-
+/**
+ * 
+ */
 public class ToFastException extends Exception{
     
     private Fahrzeugmarke obj;
-    
+   
+    /**
+     * Konstruktor
+     * @param f Fahrzeugmarke
+     */
     public ToFastException(Fahrzeugmarke f) {
         obj=f;
     }
